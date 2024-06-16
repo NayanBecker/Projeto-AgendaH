6f990d1bedfad0564963aab254fb4d97fda214af
Nayan Becker <146501593+NayanBecker@users.noreply.github.com>
2024-06-16 19:28:08 -0300

    Delete MySQL- DB - AgendaH.sql

--- MySQL- DB - AgendaH.sql
+++
@@ -1,7 +0,0 @@
-CALL inserirTarefaComNotificacao(
-    'Estudar Matemática',
-    'Revisar capítulos 1 a 3',
-    '2024-07-01 12:00:00',
-    1,  -- Supondo que o ID da recompensa seja 1
-    32   -- Supondo que o ID do perfil que adicionou a tarefa seja 1
-);
